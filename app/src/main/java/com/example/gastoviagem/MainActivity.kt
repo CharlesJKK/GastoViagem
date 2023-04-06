139d865e3d8bc0c8a03b1b1f15c2da46547b8bab
CharlesJKK <selrahcjunior@hotmail.com>
2023-04-06 14:47:34 -0300

    feat: utilizando viewBinding e a lógica do cálculo

--- app/src/main/java/com/example/gastoviagem/MainActivity.kt
+++ app/src/main/java/com/example/gastoviagem/MainActivity.kt
@@ -2,10 +2,40 @@ package com.example.gastoviagem
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import com.example.gastoviagem.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        //O override do onclick é o mesmo que utilizar assim
+        // -> binding.calculateButton.setOnClickListener { }
+        binding.calculateButton.setOnClickListener(this)
+
+    }
+
+    override fun onClick(view: View) {
+        if(view.id == R.id.calculate_button){
+            calculate()
+        }
+    }
+    private fun calculate(){
+
+        val distance = binding.distance.text.toString().toFloat()
+        val price = binding.price.text.toString().toFloat()
+        val autonomy = binding.autonomy.text.toString().toFloat()
+
+        val totalValue = (distance * price) / autonomy
+
+        binding.totalValue.text = "R$ ${"%.2f".format(totalValue)}"
+        //Toast.makeText(this, totalValue.toString(), Toast.LENGTH_SHORT).show()
     }
 }
